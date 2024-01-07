e30594c93883a1f32dd157d202a0a34817548dec
NNata2022 <nagulanata@gmail.com>
2024-01-07 16:51:11 -0600

    first commit

---
+++ src/test/java/com/tutorialsninja/qa/TestCase_Medium/RegistrationTest.java
@@ -0,0 +1,82 @@
+package com.tutorialsninja.qa.TestCase_Medium;
+
+import org.junit.AfterClass;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.tutorialsninja.qa.Pages.RegistrationPage;
+
+public class RegistrationTest {
+	public WebDriver driver;
+
+	public RegistrationPage registrationPage;
+
+
+
+	@BeforeMethod
+
+	public void setUp() {
+
+	driver = new ChromeDriver();
+
+	driver.manage().window().maximize();
+
+	registrationPage = new RegistrationPage(driver);
+
+	registrationPage.openRegistrationPage();
+
+	}
+
+
+
+	@Test
+
+	public void testSuccessfulRegistrationWithValidCredentials() {
+
+	registrationPage.enterFirstName("John");
+
+	registrationPage.enterLastName("Doe");
+
+	registrationPage.enterEmail("johnwe.doe1@example.com");
+
+	registrationPage.enterTelephone("1234567890");
+
+	registrationPage.enterPassword("password123");
+
+	registrationPage.confirmPassword("password123");
+
+	registrationPage.checkAgreeCheckbox();
+
+	registrationPage.clickContinueButton();
+
+
+
+	// Assuming there is a success message after registration
+
+	String successMessage = "Your account has been created!";
+
+	// Assert.assertTrue(driver.getPageSource().contains(), "Your Account Has Been
+
+	// Created!");
+
+	Assert.assertTrue(driver.findElement(By.xpath("//p[text() = 'Congratulations! Your new account has been successfully created!']")).isDisplayed());
+
+
+
+	}
+
+
+
+	 @AfterClass
+
+	public void tearDown() {
+
+	driver.quit();
+
+	}
+
+}
